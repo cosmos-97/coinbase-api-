a8a313e59cef5d7885631be4c2a3e210685e2712
Cosmos <cosmos>
2022-12-31 18:55:00 +0400

    initial Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod prices;
+
+use clap::Parser;
+use prices::spot_prices;
+
+#[derive(Parser, Default, Debug)]
+struct Arguments {
+    #[arg(short, long)]
+    function_name: String,
+    #[arg(short, long)]
+    currency: String,
+    #[arg(short, long)]
+    base: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    let base = args.base.to_uppercase();
+    let currency = args.currency.to_uppercase();
+
+    match &args.function_name[..] {
+        "spot_prices" => spot_prices(base, currency),
+        _ => panic!("Invalid Function Name!"),
+    }
 }
